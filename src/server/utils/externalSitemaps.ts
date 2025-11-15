874321fa491d6f94a85315bc36b0965440db990b
LyrigCats <895134298@qq.com>
2025-11-16 02:11:52 +0800

    Update externalSitemaps.ts

--- src/server/utils/externalSitemaps.ts
+++ src/server/utils/externalSitemaps.ts
@@ -1,8 +1,7 @@
 const externalSitemaps = [
   'https://blog.astrbot.app/sitemap.xml',
   'https://astrbot.tech/sitemap.xml',
-  'https://docs.astrbot.app/sitemap.xml',
-  'https://plugins.astrbot.tech/sitemap.xml'
+  'https://docs.astrbot.app/sitemap.xml'
 ]
 
 export default externalSitemaps
