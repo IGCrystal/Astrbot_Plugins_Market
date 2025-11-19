0ed00f9f16b277d1df42c945c7cb6fc3beee7269
TragicLys <IGCrystal@wenturc.com>
2025-11-19 16:31:33 +0800

    fix(login): 优化登录提示信息，简化用户协议说明

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -72,7 +72,7 @@
               本站点仅由 <a href="https://github.com/IGCrystal" target="_blank" rel="noopener noreferrer" class="author-link">IGCrystal</a> 开发的第三方插件市场，与 AstrBot 官方无关，但是你可以前往<a href="https://plugins.astrbot.app" target="_blank" rel="noopener noreferrer" class="author-link">官方网站</a>。本网站的开发仅个人练习的目的，目前它是公开的。
             </p>
             <p class="mui-login__hint">
-              本站点使用 GitHub OAuth 进行身份验证。但不会存储或访问你的 GitHub 凭据。登录即代表您同意条款。<br><br>为了更好的使用体验，请完成 GitHub 认证以继续访问。
+              为了更好的使用体验，请完成 GitHub 认证以继续访问。登录即代表您同意
               <span
                 role="link"
                 tabindex="0"
