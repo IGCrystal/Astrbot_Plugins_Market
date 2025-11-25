4418d321b8ddca560aee9587786c34aa0ac17d54
IGCrystal <IGCrystal@outlook.jp>
2025-11-25 16:48:15 +0800

    Remove obsolete sitemap URL

--- src/server/utils/externalSitemaps.ts
+++ src/server/utils/externalSitemaps.ts
@@ -1,7 +1,5 @@
 const externalSitemaps = [
-  'https://blog.astrbot.app/sitemap.xml',
   'https://astrbot.tech/sitemap.xml',
-  'https://docs.astrbot.app/sitemap.xml'
 ]
 
 export default externalSitemaps
